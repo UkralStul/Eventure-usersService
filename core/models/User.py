05cebd462979e16f15889271beddcfe7011cf8f2
UkralStul <stoyanov03@mail.ru>
2024-11-06 16:35:27 +0300

    chats models added

--- core/models/User.py
+++ core/models/User.py
@@ -1,7 +1,13 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import String
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from .base import Base
+from .conversation_user_association import conversation_user_association_table
+
+if TYPE_CHECKING:
+    from .Message import Message
+    from .Conversation import Conversation
 
 
 class User(Base):
@@ -11,3 +17,9 @@ class User(Base):
     hashed_password: Mapped[str] = mapped_column(String)
     email: Mapped[str] = mapped_column(String)
     profile_photo: Mapped[str] = mapped_column(String, nullable=True)
+
+    sent_messages: Mapped[list["Message"]] = relationship(back_populates="sender")
+    conversations: Mapped[list["Conversation"]] = relationship(
+        secondary=conversation_user_association_table,
+        back_populates="conversations",
+    )
